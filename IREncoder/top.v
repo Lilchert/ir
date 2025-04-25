85ab681981078dbcf0fe38865243a5858fe52258
Aleksei-Fil <aleks.filimonov5@gmail.com>
2025-04-25 20:27:12 +0500

    Update top.v

--- IREncoder/top.v
+++ IREncoder/top.v
@@ -7,47 +7,52 @@ module top (
 	
 reg [31:0] cmd_reg;
 wire rst = 0;
+
 wire ready;
 wire ir_out;
 reg valid;
 
-//wire [3:0] button_pressed;
+wire [3:0] button_state;
 
-//genvar i;
-//generate
-//	for (i=0; i<4; i=i+1) begin: debouncers
-//		debouncer debounce_inst (
-//		   .clk(clk25),
-//		   .rst(rst),
-//		   .button_in(key[i]),
-//		   .button_pressed(button_pressed[i]),
-//		   .button_state(),
-//		   .button_released()
-//	   	);
-//	end
-//endgenerate
-
+genvar i;
+generate
+	for (i=0; i<4; i=i+1) begin: debouncers
+		debouncer debounce_inst (
+		   .clk(clk25),
+		   .rst(rst),
+		   .button_in(key[i]),
+		   .button_pressed(),
+		   .button_state(button_state[i]),
+		   .button_released()
+	   	);
+	end
+endgenerate
+	
 always @(posedge clk25 or posedge rst) begin
 	if (rst) begin
 		cmd_reg <= 0;
 		valid   <= 1'b0;
 	end else begin
 		valid   <= 1'b0;
-		if (key[0]) begin
-			cmd_reg <= 32'b10011101011000100000011100000111; // Right
-			valid   <= 1'b1;
+		if (button_state[0]) begin
+			cmd_reg      <= 32'b10011101011000100000011100000111; // Right
+			valid        <= 1'b1;
+			button_state <= 0;
 		end
-		else if (key[1]) begin
+		else if (button_state[1]) begin
 			cmd_reg <= 32'b10011111011000000000011100000111; // Up
 			valid   <= 1'b1;
+			button_state <= 0;
 		end
-		else if (key[2]) begin
+		else if (button_state[2]) begin
 			cmd_reg <= 32'b10011110011000010000011100000111; // Down
 			valid   <= 1'b1;
+			button_state <= 0;
 		end
-		else if (key[3]) begin
+		else if (button_state[3]) begin
 			cmd_reg <= 32'b10011010011001010000011100000111; // Left
 			valid   <= 1'b1;
+			button_state <= 0;
 		end
 	end
 end
