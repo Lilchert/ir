d08ed446df80bb1e4e53382fbabcb5b465cbfe8f
Aleksei-Fil <aleks.filimonov5@gmail.com>
2025-04-26 18:01:33 +0500

    All done for FPGA

--- IREncoder/top.v
+++ IREncoder/top.v
@@ -1,6 +1,6 @@
 module top (
         input wire clk25,
-        input wire [3:0] key,
+        input wire  [3:0] key,
         output wire [0:0] led,
         output wire [0:0] gpio
 );
@@ -9,31 +9,36 @@ reg [31:0] cmd_reg;
 
 reg rst = 1'b1;
   always @(posedge clk25)
-    rst <= 1'b0;
+    if (rst == 1'b1) rst <= 1'b0;
 
 wire ready;
 wire ir_out;
+
 wire valid;
+reg valid_reg;
 
 wire [3:0] button_pressed;
 
 assign led[0] = ready;
 assign gpio[0] = ir_out;
-assign valid = key[0] | key[1] | key[2] | key[3];
+
+assign valid = valid_reg;
 
 always @(posedge clk25) begin
-                if (button_pressed[0]) begin
-                        cmd_reg      <= 32'b10011101011000100000011100000111; // Right
-                end
-                else if (button_pressed[1]) begin
-                        cmd_reg      <= 32'b10011111011000000000011100000111; // Up
-                end
-                else if (button_pressed[2]) begin
-                        cmd_reg      <= 32'b10011110011000010000011100000111; // Down
-                end
-                else if (button_pressed[3]) begin
-                        cmd_reg      <= 32'b10011010011001010000011100000111; // Left
-                end
+        if (button_pressed[0]) begin
+                cmd_reg      <= 32'b10011101011000100000011100000111; // Right
+        end
+        else if (button_pressed[1]) begin
+                cmd_reg      <= 32'b10011111011000000000011100000111; // Up
+        end
+        else if (button_pressed[2]) begin
+                cmd_reg      <= 32'b10011110011000010000011100000111; // Down
+        end
+        else if (button_pressed[3]) begin
+                cmd_reg      <= 32'b10011010011001010000011100000111; // Left
+        end
+
+        valid_reg <= |button_pressed;
 end
 
 genvar i;
@@ -61,5 +66,6 @@ ir_encoder encoder (
 );
 
 endmodule
+
 
 
